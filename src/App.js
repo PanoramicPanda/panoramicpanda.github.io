e5d0cb61fc433006d78f7b69813655b732a2ce70
PanoramicPanda <uriah.h.brown@gmail.com>
2022-08-27 18:02:05 -0400

    Big update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import avatar from './images/portrait.jpg';
 import './App.css';
+import {Chip, Grid, Stack} from "@mui/material";
+import {GitHub, LinkedIn, Twitter} from "@mui/icons-material";
+
+function InfoText(){
+    return(
+        <div className="Info">
+            <p>Welcome to my portfolio page.</p>
+            <p>It'll be under construction for a bit, but I wanted to get something basic up quick.</p>
+            <p>You can find me at the links below my portrait.</p>
+        </div>
+    );
+}
+
+function Portrait(){
+    return(
+    <div className="Avatar" >
+        <img src={avatar} className="portrait" alt="Portrait of Uriah"/>
+        {Links()}
+    </div>
+    );
+}
+
+function Links(){
+    return(
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={0.5}
+        >
+            <Grid item>
+                <Chip label="Twitter" icon={<Twitter/>} color="primary" component="a" target="_blank"
+                      href="https://twitter.com/Panoramic_Panda" clickable/>
+            </Grid>
+            <Grid item>
+                <Chip label="LinkedIn" icon={<LinkedIn/>} color="info" component="a" target="_blank"
+                      href="https://www.linkedin.com/in/uriahb/" clickable/>
+            </Grid>
+            <Grid item>
+                <Chip label="GitHub" icon={<GitHub/>} color="secondary" component="a" target="_blank"
+                      href="https://github.com/PanoramicPanda" clickable/>
+            </Grid>
+        </Grid>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Grid item lg={3} md={7} xs={7}> {InfoText()} </Grid>
+            <Grid item lg={3} md={7} xs={7}> {Portrait()} </Grid>
+        </Grid>
+
+    );
 }
 
 export default App;
